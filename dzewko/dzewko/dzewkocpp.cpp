31d3d6b3b5bc43813cf349498735d15a65d8a924
sarnecky <ssarnecki34@gmail.com>
2016-03-04 00:30:26 +0100

    Gotowe drzewo dla int

--- dzewko/dzewko/dzewkocpp.cpp
+++ dzewko/dzewko/dzewkocpp.cpp
@@ -49,8 +49,75 @@ void wypisz()
 	}
 }
 
+struct tree
+{
+	int element;
+	struct tree *left;
+	struct tree *right;
+};
+
+tree *addNode(struct tree *korzen, int n)
+{
+	if (korzen == NULL)
+	{
+		korzen = (struct tree*)malloc(sizeof(struct tree*));
+		korzen->element = n;
+		korzen->left = NULL;
+		korzen->right = NULL;
+		return korzen;
+	}
+	else
+	{
+		if (korzen->element >= n) //mniejszy-lewa strona
+		{
+			if (korzen->left == NULL)
+			{
+				korzen->left = (struct tree*)malloc(sizeof(struct tree*));
+				korzen->left->element = n;
+				korzen->left->right = NULL;
+				korzen->left->left = NULL;
+			}
+			else addNode(korzen->left, n);
+		}
+		else //wiekszy-prawa strona
+		{
+			if (korzen->right == NULL)
+			{
+				korzen->right = (struct tree*)malloc(sizeof(struct tree*));
+				korzen->right->element = n;
+				korzen->right->right = NULL;
+				korzen->right->left = NULL;
+			}
+			else addNode(korzen->right, n);
+		}
+	}
+}
+tree *a(tree *korzen)
+{
+	korzen = (struct tree*)malloc(sizeof(struct tree*));
+	cout << korzen->element;
+	return korzen;
+}
+void inOrder(tree *korzen)
+{
+	if (korzen->left != NULL) inOrder(korzen->left);
+	printf("%i, ", korzen->element);
+	if (korzen->right != NULL) inOrder(korzen->right);	
+}
 int main()
 {
+
+	
+	tree *korzen;
+	korzen = NULL;
+	korzen=addNode(korzen, 2);
+	addNode(korzen, 1);
+	addNode(korzen, 5);
+	addNode(korzen, 7);
+	addNode(korzen, 10);
+	addNode(korzen, 0);
+
+	inOrder(korzen);
 	_getch();
 	return 0;
 }
