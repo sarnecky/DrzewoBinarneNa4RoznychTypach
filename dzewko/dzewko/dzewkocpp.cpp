fad3707dc9f18070da60786d496873980947a919
sarnecky <ssarnecki34@gmail.com>
2016-03-03 21:20:08 +0100

    Proba malloc

--- dzewko/dzewko/dzewkocpp.cpp
+++ dzewko/dzewko/dzewkocpp.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-#include<conio.h>
+#include <conio.h>
+#include <memory>
+
+//calloc zeruje zarezerwowana pamiec
 using namespace std;
 
 int main()
 {
+	char *lancuch;
+	lancuch = (char *)malloc(10);
+	strcpy(lancuch, "hi");
+	printf("\nLancunch zawiera napis:%s", lancuch);
+	free(lancuch);
+
 	_getch();
 	return 0;
 }
