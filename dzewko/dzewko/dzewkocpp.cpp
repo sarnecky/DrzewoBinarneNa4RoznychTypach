cb7edac51d700d37a999220a8ba84e250bf973ff
sarnecky <ssarnecki34@gmail.com>
2016-03-06 20:28:06 +0100

    usuwanie int i double

--- dzewko/dzewko/dzewkocpp.cpp
+++ dzewko/dzewko/dzewkocpp.cpp
@@ -27,7 +27,8 @@ stack *push(int element, stack *root)
 
 	struct stack *New;
 	New = root;
-	root = (struct stack*)malloc(sizeof(struct stack*));
+	//root = (struct stack*)malloc(sizeof(struct stack*));
+	root = new stack;
 	root->next = New;
 	root->elementy_stosu = e;
 	root->number = element;
@@ -46,9 +47,12 @@ void pop(stack *root)
 	{
 		p = root;
 		root = root->next; //tutaj wierzcholek staje sie poprzednim elementem
-		free(p);
+		//free(p);
+		delete(p);
 	}
-	root->elementy_stosu = e;
+
+	if (root!=NULL)
+		root->elementy_stosu = e;
 
 }
 
@@ -75,6 +79,7 @@ struct tree
 	void *element;
 	struct tree *left;
 	struct tree *right;
+	struct tree *parent;
 	Type type;
 };
 tree *addNode(struct tree *korzen, void *n, Type type)
@@ -85,11 +90,13 @@ tree *addNode(struct tree *korzen, void *n, Type type)
 	int compareStack= 0;
 	if (korzen == NULL)
 	{
-		korzen = (struct tree*)malloc(sizeof(struct tree*));
+		//korzen = (struct tree*)malloc(sizeof(struct tree*));
+		korzen = new tree;
 		korzen->element = n;
 		korzen->left = NULL;
 		korzen->right = NULL;
 		korzen->type = type;
+		korzen->parent = korzen;
 		return korzen;
 	}
 	else
@@ -127,11 +134,13 @@ tree *addNode(struct tree *korzen, void *n, Type type)
 			{			
 				if (korzen->left == NULL)
 				{
-					korzen->left = (struct tree*)malloc(sizeof(struct tree*));
+					//korzen->left = (struct tree*)malloc(sizeof(struct tree*));
+					korzen->left = new tree;
 					korzen->left->element = n;
 					korzen->left->right = NULL;
 					korzen->left->left = NULL;
 					korzen->left->type = type;
+					korzen->left->parent = korzen;
 				}
 				else addNode(korzen->left, n, type);
 			}
@@ -141,11 +150,13 @@ tree *addNode(struct tree *korzen, void *n, Type type)
 			{
 				if (korzen->right == NULL)
 				{
-					korzen->right = (struct tree*)malloc(sizeof(struct tree*));
+					//korzen->right = (struct tree*)malloc(sizeof(struct tree*));
+					korzen->right = new tree;
 					korzen->right->element = n;
 					korzen->right->right = NULL;
 					korzen->right->left = NULL;
 					korzen->right->type = type;
+					korzen->right->parent = korzen;
 				}
 				else addNode(korzen->right, n, type);
 			}
@@ -190,6 +201,115 @@ void inOrder(tree *korzen)
 
 	if (korzen->right != NULL) inOrder(korzen->right);	
 }
+
+tree *search(tree *korzen, void *n, Type type)
+{
+	//funkcja zwraca adres gdzie znajduje sie dana wartoœæ w drzewie
+	double nowy;
+	double wezel;
+	int compareChar = 0;
+	int compareStack = 0;
+	if (korzen == NULL)
+		return korzen;
+
+
+	//schodzimy wg³¹b drzewa
+	if ((korzen->type == INT || korzen->type == DOUBLE || korzen->type == CHAR || korzen->type == STOS) && (type == INT || type == DOUBLE || type == CHAR || type == STOS))
+	{
+		//przypadki int i double
+		if (korzen->type == INT) wezel = *static_cast<int*>(korzen->element);
+		else wezel = *static_cast<double*>(korzen->element);
+
+		if (type == DOUBLE) nowy = *static_cast<double*>(n);
+		else nowy = *static_cast<int*>(n);
+
+		if (type == korzen->type)
+			if ((double)wezel == (double)nowy)
+				return korzen;
+
+		//przypadek char*
+		if (korzen->type == CHAR && type == CHAR) //przypadek gdy porwnujemy 2 chary
+		{
+
+			char *w = static_cast<char*>(korzen->element); //element sprawdzany
+			char *no = static_cast<char*>(n); //nowy element
+			if (strcmp(no, w) > 0) compareChar = 1;//nowy wiekszy, idzie na prawo
+			else compareChar = 2;//nowy mniejszy idzie na lewo
+		}
+
+		//przypadek stosu
+		if (korzen->type == STOS && type == STOS) //przypadek gdy porownujemy 2 stosy
+		{
+			stack *w = static_cast<stack*>(korzen->element);
+			stack *no = static_cast<stack*>(n);
+			if (w->elementy_stosu < no->elementy_stosu) compareStack = 1; //ilosc elemetow nowego stosu jest wieksza wiec wstwiamy go do prawego wezla
+			else compareStack = 2;
+		}
+
+		//logika wyszukiwania
+
+		if ((korzen->type == CHAR && type != CHAR && type != STOS) ||
+			((double)wezel >= (double)nowy && (korzen->type != CHAR && type != CHAR && korzen->type != STOS && type != STOS)) ||
+			compareChar == 2 || compareStack == 2 || (korzen->type == STOS && type != STOS)) //mniejszy-lewa strona
+		{
+			return search(korzen->left, n, type); //schodzimy w lewo stronê
+		}
+		else if ((korzen->type != CHAR && type == CHAR && korzen->type != STOS) ||
+			((double)wezel < (double)nowy && (korzen->type != CHAR && type != CHAR)) ||
+			compareChar == 1 || compareStack == 1 || (korzen->type != STOS && type == STOS)) //wiekszy-prawa strona
+		{
+			return search(korzen->right, n, type); //schodzimy w praw¹ stronê
+		}
+		else{}
+	}
+
+}
+tree *searchMin(tree *korzen)
+{
+	while (korzen->left != NULL)
+		korzen = korzen->left;
+	return korzen;
+}
+tree *nastepnik(tree *korzen)
+{
+	if (korzen->right != NULL)
+		return searchMin(korzen);
+	tree *tmp = korzen->parent;
+	while (tmp != NULL && tmp->left != korzen)
+	{
+		korzen = tmp;
+		tmp = tmp->parent;
+	}
+	return tmp;
+}
+tree *deleteNode(struct tree *korzen, void *n, Type type)
+{
+	tree *DoUsuniecia = search(korzen, n, type);
+	tree *y, *x;
+	if (DoUsuniecia->left == NULL || DoUsuniecia->right == NULL)
+		y = DoUsuniecia;
+	else y = nastepnik(DoUsuniecia);
+
+	if (y->left != NULL)
+		x = y->left;
+	else
+		x = y->right;
+	if (x != NULL)
+		x->parent = y->parent;
+	if (y->parent == NULL)
+		korzen->parent = x; //korzen->root !!!!!
+	else
+	{
+		if (y == y->parent->left)
+			y->parent->left = x;
+		else
+			y->parent->right = x;
+	}
+
+	if (y != DoUsuniecia)
+		DoUsuniecia->element = y->element;
+	return y;
+}
 void interfejs()
 {
 	int b = 10;
@@ -198,11 +318,12 @@ void interfejs()
 	korzen = NULL;
 	korzen = addNode(korzen, &b, INT);
 	int k, ilosc;
-	int *w;
-	double *wd;
+	int *w,a0;
+	double *wd,a1;
+	int *z;
 	stack *s = NULL;
 	cout << "Nacisnij klawisz\n";
-	cout << "1. int, 2. double, 3. char*, 4. stos, 5. podglad drzewa, 6. wyjscie z danej instrukcji\n";
+	cout << "1. int, 2. double, 3. char*, 4. stos, 5. podglad drzewa, 6. usun dany wezel \n";
 	while (cin >> k)
 	{
 		system("cls");
@@ -227,7 +348,7 @@ void interfejs()
 			{
 				for (int i = 0; i < ilosc; i++)
 				{
-
+					
 					cout << "Jaki znak ?";
 					cin >> w[i];
 					addNode(korzen, &w[i], INT);
@@ -293,29 +414,52 @@ void interfejs()
 
 			inOrder(korzen);
 			break;
+		case 6:
+			cout << "Podaj typ zmiennej 0. INT 1.DOUBLE 2.CHAR* 3.STOS \n";
+			cin >> k;
+			cout << "Podaj wartosc zmiennej/stosu \n";
+			if (k == 0)
+			{
+				cin >> a0;
+				delete(deleteNode(korzen, &a0, (Type)k));
+			}
+			if (k == 1)
+			{
+				cin >> a1;
+				delete(deleteNode(korzen, &a1, (Type)k));
+			}
+			if (k == 2)
+			{
+				napis = new char;
+				cin >> napis;
+				delete(deleteNode(korzen, napis, (Type)k));
+			}
+			system("cls");
+			inOrder(korzen);
+			break;
+
 		}
 		cout << "Nacisnij klawisz\n";
-		cout << "1. int, 2. double, 3. char*, 4. stos, 5. podglad drzewa, 6. wyjscie z danej instrukcji\n";
+		cout << "1. int, 2. double, 3. char*, 4. stos, 5. podglad drzewa, 6.usun element\n";
 	}
 
-}
+}	
 int main()
 {
 	int b = 10;
 	char *napis;
-	//double c = 4.1;
-	//void *w = &b;
-	//char *cz = "abz";
-	//char *cz2 = "aba";
-	//stack *s = NULL;
-	//s=push(2,s);
-	//s=push(3,s);
-	//s=push(5,s);
-	////wypisz(s);
-	//stack *s2 = NULL;
-	//s2=push(5, s2);
-	//s2=push(9, s2);
-	/////wypisz(s2);
+	double c = 4.1;
+	void *w = &b;
+	char *cz = "abz";
+	char *cz2 = "aba";
+	stack *s = NULL;
+	s=push(2,s);
+	pop(s);
+	//wypisz(s);
+	stack *s2 = NULL;
+	s2=push(5, s2);
+	s2=push(9, s2);
+	///wypisz(s2);
 	tree *korzen;
 	korzen = NULL;
 	////uwazac na zmiane adresow zmiennych
@@ -331,7 +475,7 @@ int main()
 	//double h = 11.11;
 	//addNode(korzen, &h, DOUBLE);
 
-	
+	interfejs();
 
 	//inOrder(korzen);
 	_getch();
